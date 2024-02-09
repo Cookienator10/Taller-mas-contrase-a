784ea690d1e22d038320d36bbf5fc53a5a230142
Santiago Rojas Rodríguez <santiagorojas772@gmail.com>
2024-02-09 10:56:32 -0500

    se arreglo js contraseñas.

--- Contraseña/c.js
+++ Contraseña/c.js
@@ -10,7 +10,7 @@ button.addEventListener('click', (e) => {
 
     const longitud_minima = 8;
 
-    if (validarClave(data.p, longitud_minima)) {
+    if (validarClave(data.p, longitud_minima) && signs(data.p)) {
         alert("La contraseña es aceptable.");
     } else {
         alert("La contraseña no cumple con los requisitos.");
@@ -18,20 +18,17 @@ button.addEventListener('click', (e) => {
 });
 
 function validarClave(clave, longitudMinima) {
-    // Verificar longitud mínima
     if (clave.length < longitudMinima) {
         return false;
     }
-
-    // Convertir la primera letra a mayúscula
-    clave = clave.charAt(0).toUpperCase() + clave.slice(1);
-
-    // Verificar la presencia de al menos uno de los caracteres: * / + -
-    if (!/[*/+-]/.test(clave)) {
-        return false;
-    }
-
-    // La clave cumple con todos los requisitos
     return true;
 }
+
+function signs(p) {
+    let expresionRegular = /^[*/+]+$/;
+    return expresionRegular.test(p);
+}
+
+
+
 
